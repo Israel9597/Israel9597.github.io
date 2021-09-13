e69d06452e451c9f5c4c4de0e62f97f8a38c1fd1
Israel Haile <84593561+Israel9597@users.noreply.github.com>
2021-09-12 21:44:56 -0500

    array practice

--- d25destructure/arrayPractice.js
+++ d25destructure/arrayPractice.js
@@ -1,21 +1,26 @@
 "use strict";
 /* eslint-disable */
 /* You need the module.exports when testing in node.  Comment it out when you send your file to the browser */
-// module.exports = {
-//   doubleNums,
-//   doubleAges,
-//   filterEven,
-//   filterOver10,
-//   findEvenNum,
-//   findEvenAge,
-//}; //add all of your function names here that you need for the node mocha tests
+module.exports = {
+  doubleNums,
+  doubleAges,
+  filterEven,
+  filterOver10,
+  findEvenNum,
+  findEvenAge,
+}; //add all of your function names here that you need for the node mocha tests
 
 function doubleNums(arr) {
   return arr.map((num) => num * 2);
 }
 
 function doubleAges(arr) {
-  return arr.map((item) => item.age * 2);
+  return arr.map((item) => {
+    let name = item.name;
+    let age = item.age * 2;
+    let obj = { name, age };
+    return obj;
+  });
 }
 
 function filterEven(arr) {
