c4913f7e2db7542872fd2bc1cceabb3140ad1cb5
Israel Haile <84593561+Israel9597@users.noreply.github.com>
2021-09-14 19:56:10 -0500

    recursion

---
+++ d31recursion/recursion.js
@@ -0,0 +1,38 @@
+"use strict";
+
+/* global exports */
+/* You need the module.exports when testing in node.  Comment it out when you send your file to the browser
+ */
+// module.exports = {
+//   sumTo,
+//   factorial,
+//   fibonacci,
+//   outputList,
+//   outputListLoop,
+//   reverseList,
+//   reverseListLoop,
+//}; //add all of your function names here that you need for the node mocha tests
+
+function sumTo(num) {
+  if (num < 1) {
+    return 0;
+  } else {
+    return num + sumTo(num - 1);
+  }
+}
+
+function factorial(num) {
+  if (num === 1) {
+    return 1;
+  } else {
+    return num * factorial(num - 1);
+  }
+}
+
+function fibonacci(num) {
+  if (num < 2) {
+    return num;
+  } else {
+    return fibonacci(num - 1) + fibonacci(num - 2);
+  }
+}
