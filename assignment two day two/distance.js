50f8e0cbd2d1c01166a477ad215a606345685de4
Israel Haile <84593561+Israel9597@users.noreply.github.com>
2021-09-27 14:56:47 -0500

    back up

--- assignment two day two/distance.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-
-function calcDistance(x1, y1, x2, y2) {
-  const d = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
-  return console.log(d);
-}
-calcDistance(0, 0, 5, 5);
--- assignment two day two/fahrenheit.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-function convertFahrenheit(far) {
-  const celcius = (far - 32) / 1.8;
-  return console.log(celcius);
-}
-convertFahrenheit(0);
